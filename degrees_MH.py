bee55c415f1d7541791d5611d2f94b91066cb217
Manuel <manuelheller@outlook.com>
2020-07-10 22:09:28 +0200

    Progress degree

--- degrees_MH.py
+++ degrees_MH.py
@@ -1,6 +1,8 @@
 import csv
 import sys
 
+from util import Node, StackFrontier, QueueFrontier
+
 # Maps names to a set of corresponding person_ids
 names = {}
 
@@ -49,13 +51,114 @@ def load_data(directory):
             except KeyError:
                 pass
 
+
+def main():
+    if len(sys.argv) > 2:
+        sys.exit("Usage: python degrees.py [directory]")
+    directory = sys.argv[1] if len(sys.argv) == 2 else "large"
+
+    # Load data from files into memory
+    print("Loading data...")
+    load_data(directory)
+    print("Data loaded.")
+
+    source = person_id_for_name(input("Name: "))
+    if source is None:
+        sys.exit("Person not found.")
+    target = person_id_for_name(input("Name: "))
+    if target is None:
+        sys.exit("Person not found.")
+
+    path = shortest_path(source, target)
+
+    if path is None:
+        print("Not connected.")
+    else:
+        degrees = len(path)
+        print(f"{degrees} degrees of separation.")
+        path = [(None, source)] + path
+        for i in range(degrees):
+            person1 = people[path[i][1]]["name"]
+            person2 = people[path[i + 1][1]]["name"]
+            movie = movies[path[i + 1][0]]["title"]
+            print(f"{i + 1}: {person1} and {person2} starred in {movie}")
+
+
+def shortest_path(source, target):
+    """
+    Returns the shortest list of (movie_id, person_id) pairs
+    that connect the source to the target.
+
+    If no possible path, returns None.
+    """
+
+    # TODO
+    raise NotImplementedError
+
+
+def person_id_for_name(name):
+    """
+    Returns the IMDB id for a person's name,
+    resolving ambiguities as needed.
+    """
+    person_ids = list(names.get(name.lower(), set()))
+    if len(person_ids) == 0:
+        return None
+    elif len(person_ids) > 1:
+        print(f"Which '{name}'?")
+        for person_id in person_ids:
+            person = people[person_id]
+            name = person["name"]
+            birth = person["birth"]
+            print(f"ID: {person_id}, Name: {name}, Birth: {birth}")
+        try:
+            person_id = input("Intended Person ID: ")
+            if person_id in person_ids:
+                return person_id
+        except ValueError:
+            pass
+        return None
+    else:
+        return person_ids[0]
+
+
+def neighbors_for_person(person_id):
+    """
+    Returns (movie_id, person_id) pairs for people
+    who starred with a given person.
+    """
+    movie_ids = people[person_id]["movies"]
+    neighbors = set()
+    for movie_id in movie_ids:
+        for person_id in movies[movie_id]["stars"]:
+            neighbors.add((movie_id, person_id))
+    return neighbors
+
+
 directory = 'small'
 
+print("Loading data...")
 load_data(directory)
+print("Data loaded.")
 
+source = person_id_for_name(input("Name: "))
+if source is None:
+    sys.exit("Person not found.")
+target = person_id_for_name(input("Name: "))
+if target is None:
+    sys.exit("Person not found.")
+
+print('names')
 print(type(names))
 print(names)
+print('movies')
 print(type(movies))
 print(movies)
+print('people')
 print(type(people))
 print(people)
+
+print("neighbors source")
+print(neighbors_for_person(source))
+print("neighbors target")
+print(neighbors_for_person(target))
