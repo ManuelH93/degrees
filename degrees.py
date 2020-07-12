08120b38c183e4921936bf863bad36ab440a18fe
Manuel <manuelheller@outlook.com>
2020-07-12 10:14:19 +0200

    Complete project

--- degrees.py
+++ degrees.py
@@ -92,9 +92,42 @@ def shortest_path(source, target):
     If no possible path, returns None.
     """
 
-    # TODO
-    raise NotImplementedError
+    # Initialize frontier to just the starting position
+    start = Node(state=source, parent=None, action=None)
+    frontier = QueueFrontier()
+    frontier.add(start)
 
+    # Initialize an empty explored set
+    explored = set()
+
+    # Keep looping until solution found
+    while True:
+
+        # If nothing left in frontier, then no path
+        if frontier.empty():
+            path = None
+            return path
+
+        # Choose a node from the frontier
+        node = frontier.remove()
+        
+        # Mark node as explored
+        explored.add(node.state)
+
+        # Add neighbors to frontier
+        for movie_id, person_id in neighbors_for_person(node.state):
+            if not frontier.contains_state(person_id) and person_id not in explored:
+                child = Node(state=person_id, parent=node, action=movie_id)
+                frontier.add(child)
+                # If child node is the goal, then we have a solution
+                if child.state == target:
+                    node = child
+                    path = []
+                    while node.parent is not None:
+                        path.append((node.action, node.state))
+                        node = node.parent
+                    path.reverse()
+                    return path
 
 def person_id_for_name(name):
     """
